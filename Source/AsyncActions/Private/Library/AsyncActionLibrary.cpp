0d682f12a722a354307a88afe96ec7c66a40ff95
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-10-09 01:07:21 +0300

    Add function library in async actions module

---
+++ Source/AsyncActions/Private/Library/AsyncActionLibrary.cpp
@@ -0,0 +1,5 @@
+// This is Sandbox Project. 
+
+
+#include "Library/AsyncActionLibrary.h"
+
---
+++ Source/AsyncActions/Public/Library/AsyncActionLibrary.h
@@ -0,0 +1,17 @@
+// This is Sandbox Project. 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AsyncActionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASYNCACTIONS_API UAsyncActionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
