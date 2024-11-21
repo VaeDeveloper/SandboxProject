77bc519588721cef864a987daa97429316922954
VaeDeveloper <dmitriyalexdev@gmail.com>
2024-11-21 18:16:07 +0300

    add cpp anim instance class and blueprint inherit

---
+++ Source/SandboxProject/SandboxProjectAnimInstance.cpp
@@ -0,0 +1,26 @@
+// This is Sandbox Project. 
+
+
+#include "SandboxProjectAnimInstance.h"
+
+USandboxProjectAnimInstance::USandboxProjectAnimInstance(const FObjectInitializer& ObjectInitializer) 
+{
+
+}
+
+void USandboxProjectAnimInstance::NativeInitializeAnimation() 
+{
+
+}
+
+void USandboxProjectAnimInstance::NativeUpdateAnimation(float DeltaSeconds) 
+{
+
+}
+
+void USandboxProjectAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds) 
+{
+
+}
+
+void USandboxProjectAnimInstance::NativeBeginPlay() {}
